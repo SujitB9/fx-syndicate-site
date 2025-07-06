59afc4f6195275ecd8aee6a04cab50d8ae7e4a60
SujitB9 <sujeetborade12201@gmail.com>
2025-07-06 14:04:42 +0530

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,11 +1,9 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './Home'
-import './index.css'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
